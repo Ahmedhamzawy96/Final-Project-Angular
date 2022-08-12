8dfdcf98bdc0e10f6157b6383759426a28e7f0f2
AyaMohmmed <ayamohmmed967@gmail.com>
2022-08-12 15:56:35 +0200

    car

--- src/app/Components/car-data/car-data.component.ts
+++ src/app/Components/car-data/car-data.component.ts
@@ -2,25 +2,63 @@ import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ICar } from 'src/app/Interface/ICar';
 import { IExportProduct } from 'src/app/Interface/IExportProduct';
+import { CarService } from 'src/app/Services/Car/car.service';
 import { ExportProductService } from 'src/app/Services/ExportProduct/export-product.service';
+import{ FormGroup,FormControl,Validators} from '@angular/forms'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-car-data',
   templateUrl: './car-data.component.html',
   styleUrls: ['./car-data.component.css']
 })
+
+
 export class CarDataComponent implements OnInit {
  car:IExportProduct[];
-  constructor(private src:ExportProductService) { 
-    //this.car=["dmx","nissan","ch","toyota"];
+
+ //1
+ carsdata : ICar[];
+
+ cardataform :FormGroup ;
+
+
+  constructor(private src:ExportProductService,private carserv :CarService,private route:ActivatedRoute)
+   { 
+
+   this.cardataform = new FormGroup({
+    
+      id:new FormControl({value:'',disabled:true}),
+      name:new FormControl('',[Validators.required]),
+      account:new FormControl('',[Validators.required]),
+      notes:new FormControl('',[Validators.required]),
+      
+    });
+
+    console.log(this.cardata);
+
+    this.ngOnInit();
   }
 
   ngOnInit(): void { 
-      //  this.src.getReciepts().subscribe((Date=> {
-      //   this.car=Date
-      //   console.log(this.car);
-    // }))
-    
+     
+    //2
+    this.carserv.getCar().subscribe(Date=>{this.carsdata = Date; console.log(this.carsdata);console.log(Date)} )
+  }
+//
+  delete (carsdata:ICar[]){}
+//
+ 
+  cardata(){
+if (this.cardataform.valid){
+  this.carserv.addCar(this.cardataform.value).subscribe(data=>{    this.carserv.getCar().subscribe(Date=>{this.carsdata = Date; console.log(this.carsdata);console.log(Date)} )
+})
+
+}
+  } 
+  viwe(){
+    console.log (this.cardataform.value)
   }
 
+  
 }
--- src/app/Components/cars-accounts/cars-accounts.component.ts
+++ src/app/Components/cars-accounts/cars-accounts.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { ICar } from 'src/app/Interface/ICar';
+import { IExportProduct } from 'src/app/Interface/IExportProduct';
+import { CarService } from 'src/app/Services/Car/car.service';
+import { ExportProductService } from 'src/app/Services/ExportProduct/export-product.service';
+import{ FormGroup,FormControl,Validators} from '@angular/forms'
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-cars-accounts',
   templateUrl: './cars-accounts.component.html',
   styleUrls: ['./cars-accounts.component.css']
 })
+
 export class CarsAccountsComponent implements OnInit {
 
-  constructor() { }
+  car:IExportProduct[];
 
-  ngOnInit(): void {
+  caraccount : ICar[];
+
+  caraccountform :FormGroup ;
+
+  caraccountformsc :FormGroup ;
+
+  constructor(private src:ExportProductService,private caracountserv :CarService,private route:ActivatedRoute)
+   { 
+    this.caraccountform = new FormGroup({
+    
+      id:new FormControl({value:'',disabled:true}),
+      name:new FormControl('',[Validators.required]),
+      // account:new FormControl('',[Validators.required]),
+      notes:new FormControl('',[Validators.required]),
+      
+    });
+
+    this.caraccountformsc = new FormGroup({
+    
+      id:new FormControl({value:'',disabled:true}),
+      notes:new FormControl('',[Validators.required]),
+      
+    });
+   }
+
+  ngOnInit(): void 
+  {
+    this.caracountserv.getCar().subscribe(Date=>{this.caraccount = Date; console.log(this.caraccount);console.log(Date)} )
+
   }
-
+  
+  cardata(){
+    if (this.caraccountform.valid){
+      this.caracountserv.addCar(this.caraccountform.value).subscribe(data=>{this.caracountserv.getCar().subscribe(Date=>{this.caraccount = Date; console.log(this.caraccount);console.log(Date)} )
+    })
+    
+    }
+      } 
+      cardatasc(){
+        if (this.caraccountformsc.valid){
+          this.caracountserv.addCar(this.caraccountformsc.value).subscribe(data=>{this.caracountserv.getCar().subscribe(Date=>{this.caraccount = Date; console.log(this.caraccount);console.log(Date)} )
+        })
+        
+        }
+      }
 }
