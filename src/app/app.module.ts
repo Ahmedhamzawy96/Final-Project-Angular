0e311e1d56af1bd807f5445c8662e348fd4fbeed
Ahmed Hamzawy <96156858+Ahmedhamzawy96@users.noreply.github.com>
2022-08-21 06:48:45 +0200

    Refunds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,16 +32,20 @@ import { UsersComponent } from './Components/users/users.component';
 import { ReportsComponent } from './Components/Reports-Folder/reports-main/reports.component';
 import { StartComponent } from './Components/start/start.component';
 import { ErorrComponent } from './Components/erorr/erorr.component';
-import { ExportRecRefundComponent } from './Components/export-rec-refund/export-rec-refund.component';
+import { ExportRecRefundComponent } from './Components/Refunds/export-rec-refund/export-rec-refund.component';
 import { HomeComponent } from './Components/home/home.component';
 import { CarRecPrintComponent } from './Components/car-rec-print/car-rec-print.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { ReportCustomerAccountsComponent } from './Components/Reports-Folder/report-customer-accounts/report-customer-accounts.component';
 import { ReceiptfromcarComponent } from './Components/receiptfromcar/receiptfromcar.component';
 import { ReportCustomerReceiptsComponent } from './Components/Reports-Folder/report-customer-receipts/report-customer-receipts.component';
 import { ReportsSupplierReceiptComponent } from './Components/Reports-Folder/reports-supplier-receipt/reports-supplier-receipt.component';
 import { ReportsSupplierAccountComponent } from './Components/Reports-Folder/reports-supplier-account/reports-supplier-account.component';
+import { ImportRecRefundComponent } from './Components/Refunds/import-rec-refund/import-rec-refund.component';
+import { FromCarRefundComponent } from './Components/Refunds/from-car-refund/from-car-refund.component';
+import { ToCarRefundComponent } from './Components/Refunds/to-car-refund/to-car-refund.component';
+import { MainRefundComponent } from './Components/Refunds/main-refund/main-refund.component';
 
 @NgModule({
   declarations: [
@@ -77,6 +81,10 @@ import { ReportsSupplierAccountComponent } from './Components/Reports-Folder/rep
     ReportCustomerReceiptsComponent,
     ReportsSupplierReceiptComponent,
     ReportsSupplierAccountComponent,
+    ImportRecRefundComponent,
+    FromCarRefundComponent,
+    ToCarRefundComponent,
+    MainRefundComponent,
   ],
   imports: [
     BrowserModule,
